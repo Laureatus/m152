0ebea64429e9a925916c0722adaffdde765bd54c
Laureatus <lorin.fankhauser@gmail.com>
2023-05-11 08:35:04 +0200

    Add Cypress tests

---
+++ cypress/e2e/form.cy.js
@@ -0,0 +1,13 @@
+describe('check if website can be reached', () => {
+    it('passes', () => {
+      cy.visit('https://laureatus.github.io/m152/form.html')
+      cy.get('#demo-name').type('John Doe')
+      cy.get('#demo-email').type('john.doe@example')
+      cy.get('demo-category').select('Administration')
+      cy.get('#demo-priority-normal').click()
+      cy.get('#demo-copy').click()
+      cy.get('#demo-human').click()
+      cy.get('#demo-message').type('Hello World')
+      cy.get('.actions:nth-child(2)').click()
+    })
+  })
--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,5 +1,91 @@
-describe('empty spec', () => {
+describe('check if website can be reached', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://laureatus.github.io/m152/index.html')
   })
 })
+
+describe('check if mockups.html can be reached', () => {
+  it('passes', () => {
+      cy.visit('https://laureatus.github.io/m152/mockups.html')
+  })
+})
+
+describe('check if styleguide.html can be reached', () => {
+  it('passes', () => {
+      cy.visit('https://laureatus.github.io/m152/styleguide.html')
+  })
+})
+
+describe('check if urheberrecht.html can be reached', () => {
+  it('passes', () => {
+      cy.visit('https://laureatus.github.io/m152/urheberrecht.html')
+  })
+})
+
+describe('check if copyright.html can be reached', () => {
+  it('passes', () => {
+      cy.visit('https://laureatus.github.io/m152/copyright.html')
+  })
+})
+
+describe('check if dsgvo.html can be reached', () => {
+  it('passes', () => {
+      cy.visit('https://laureatus.github.io/m152/dsgvo.html')
+  })
+})
+
+describe('check if bilder.html can be reached', () => {
+  it('passes', () => {
+      cy.visit('https://laureatus.github.io/m152/bilder.html')
+  })
+})
+
+describe('check if invert-shadow.html can be reached', () => {
+  it('passes', () => {
+      cy.visit('https://laureatus.github.io/m152/invert-shadow.html')
+  })
+})
+
+describe('check if round-corner.html can be reached', () => {
+  it('passes', () => {
+      cy.visit('https://laureatus.github.io/m152/round-corner.html')
+  })
+})
+
+describe('check if invert-border.html can be reached', () => {
+  it('passes', () => {
+      cy.visit('https://laureatus.github.io/m152/invert-border.html')
+  })
+})
+
+describe('check if svg.html can be reached', () => {
+  it('passes', () => {
+      cy.visit('https://laureatus.github.io/m152/svg.html')
+  })
+})
+
+describe('check if keyframes.html can be reached', () => {
+  it('passes', () => {
+      cy.visit('https://laureatus.github.io/m152/keyframes.html')
+  })
+})
+
+describe('check if canvas.html can be reached', () => {
+  it('passes', () => {
+      cy.visit('https://laureatus.github.io/m152/canvas.html')
+  })
+})
+
+describe('check if scroll-animations.html can be reached', () => {
+  it('passes', () => {
+      cy.visit('https://laureatus.github.io/m152/scroll-animations.html')
+  })
+})
+
+describe('check if impressum.html can be reached', () => {
+  it('passes', () => {
+      cy.visit('https://laureatus.github.io/m152/impressum.html')
+  })
+})
+
+
