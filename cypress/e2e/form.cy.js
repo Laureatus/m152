efe9d55fb90e7db91c5690336463857ba3fb8a92
Laureatus <lorin.fankhauser@gmail.com>
2023-05-11 08:44:06 +0200

    Update Cypress tests

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -5,8 +5,10 @@ describe('check if website can be reached', () => {
       cy.get('#demo-email').type('john.doe@example')
       cy.get('#demo-category').select('Administration')
       cy.get('#demo-priority-normal').click()
-      cy.get('#demo-copy').click()
-      cy.get('#demo-human').click()
+      cy.get('#demo-copy').invoke('show');
+      cy.get('#demo-copy').click({ force: true })
+      cy.get('#demo-human').invoke('show');
+      cy.get('#demo-human').click({ force: true })
       cy.get('#demo-message').type('Hello World')
       cy.get('.actions:nth-child(2)').click()
     })
