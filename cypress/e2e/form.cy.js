17cc4500e04a4135e9117151449b7a71ceddf8a1
Laureatus <lorin.fankhauser@gmail.com>
2023-05-11 08:50:00 +0200

    Update Cypress tests

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -1,15 +1,27 @@
-describe('check if website can be reached', () => {
-    it('passes', () => {
+describe('Check if form can be filled out and reset', () => {
+    it('checks reset button', () => {
       cy.visit('https://laureatus.github.io/m152/form.html')
       cy.get('#demo-name').type('John Doe')
       cy.get('#demo-email').type('john.doe@example')
       cy.get('#demo-category').select('Administration')
-      cy.get('#demo-priority-normal').click()
-      cy.get('#demo-copy').invoke('show');
-      cy.get('#demo-copy').click({ force: true })
-      cy.get('#demo-human').invoke('show');
-      cy.get('#demo-human').click({ force: true })
+      cy.get(':nth-child(5) > label').click()
+      cy.get(':nth-child(7) > label').click()
+      cy.get(':nth-child(8) > label').click()
       cy.get('#demo-message').type('Hello World')
-      cy.get('.actions:nth-child(2)').click()
+      cy.get('.actions > :nth-child(2) > input').click()
+    })
+  })
+
+  describe('Check if form can be filled out and submitted', () => {
+    it('checks reset button', () => {
+      cy.visit('https://laureatus.github.io/m152/form.html')
+      cy.get('#demo-name').type('John Doe')
+      cy.get('#demo-email').type('john.doe@example')
+      cy.get('#demo-category').select('Administration')
+      cy.get(':nth-child(5) > label').click()
+      cy.get(':nth-child(7) > label').click()
+      cy.get(':nth-child(8) > label').click()
+      cy.get('#demo-message').type('Hello World')
+      cy.get('.primary').should('exist')
     })
   })
